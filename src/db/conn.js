0b62b5e90d58cb521cdb9c14c1be75f3af0f123a
amansingh4545 <133216664+amansingh4545@users.noreply.github.com>
2023-06-28 16:23:13 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/registration", {useNewUrlParser : true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://Amansingh:amansingh245172@cluster.za7ia9z.mongodb.net/reg", {useNewUrlParser : true, useUnifiedTopology: true})
 .then(()=>console.log("connection success"))
 .catch((err)=> console.log(err));
