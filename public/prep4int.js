4323d9714a5a78d2bbc0f21db248529b47d2e5ab
amansingh4545 <133216664+amansingh4545@users.noreply.github.com>
2023-12-23 01:30:22 +0530

    Update prep4int.js

--- public/prep4int.js
+++ public/prep4int.js
@@ -1,11 +1,10 @@
-const but1= document.querySelector('.but1');
-const but2= document.querySelector('.but2');
-const but3= document.querySelector('.but3');
-const send= document.querySelector('.submit-button');
+const x = document.getElementsByClassName("button");//querySelector me '.button' karte hai isme directly
+for(var i=0; i<x.length; i++){
+  x[i].style.backgroundColor='rgb(242, 229, 210)';
+  x[i].style.borderStyle='outset';
+}
 
-but1.style.backgroundColor='rgb(243, 238, 231)';
-but2.style.backgroundColor='rgb(243, 238, 231)';
-but3.style.backgroundColor='rgb(243, 238, 231)';
+const send= document.querySelector('.submit-button');
 send.style.backgroundColor='#F44770';
 
 send.addEventListener('mouseover', ()=>{
@@ -17,37 +16,17 @@ send.addEventListener('mouseout', ()=>{
   send.style.transition='.2s ease';
 })
 
-but1.addEventListener('mouseover', ()=>{
-  but1.style.backgroundColor='rgb(131, 245, 144)';
-  // but1.style.boxShadow='0 0 40px #a167ed';
-  but1.style.transition='.2s ease';
+// but1.addEventListener('mouseover', ()=>{
+//   but1.style.backgroundColor='rgb(131, 245, 144)';
+//   // but1.style.boxShadow='0 0 40px #a167ed';
+//   but1.style.transition='.2s ease';
   
-})
-but2.addEventListener('mouseover', ()=>{
-  but2.style.backgroundColor='rgb(131, 245, 144)';
-  // but2.style.boxShadow='0 0 40px #a167ed';
-  but2.style.transition='.2s ease';
-})
-but3.addEventListener('mouseover', ()=>{
-  but3.style.backgroundColor='rgb(131, 245, 144)';
-  // but2.style.boxShadow='0 0 40px #a167ed';
-  but3.style.transition='.2s ease';
-})
-but1.addEventListener('mouseout', ()=>{
-  but1.style.backgroundColor='rgb(243, 238, 231)';
-  but1.style.boxShadow='0 0 40px white';
-  but1.style.transition='.2s ease';
-})
-but2.addEventListener('mouseout', ()=>{
-  but2.style.backgroundColor='rgb(243, 238, 231)';
-  but2.style.boxShadow='0 0 40px white';
-  but2.style.transition='.2s ease';
-})
-but3.addEventListener('mouseout', ()=>{
-  but3.style.backgroundColor='rgb(243, 238, 231)';
-  but3.style.boxShadow='0 0 40px white';
-  but3.style.transition='.2s ease';
-})
+// })
+// but1.addEventListener('mouseout', ()=>{
+//   but1.style.backgroundColor='cyan';
+//   but1.style.boxShadow='0 0 40px white';
+//   but1.style.transition='.2s ease';
+// })
 
 
 
